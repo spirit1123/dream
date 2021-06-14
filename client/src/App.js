4670b46b58d3f98fd9d5dfa6f263d4e7743515a8
Christian Dirnberger <christian.dirnberger@stroeer.de>
2021-06-14 23:46:43 +0200

    actionbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { Component } from 'react';
 import './App.css';
 import { World } from './components/game/World';
+import { ActionBar } from "./components/ui/ActionBar";
 
 export class App extends Component {
   render() {
     return (
       <div className="App">
         <World></World>
+        <ActionBar></ActionBar>
       </div>
     );
   }
---
+++ client/src/components/ui/ActionBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class ActionBar extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Lalilu</p>
+            </div>
+        )
+    }
+}
