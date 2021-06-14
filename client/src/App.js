904a73fe848d139e0bfd1f20053d5b39097642b6
Christian Dirnberger <christian.dirnberger@stroeer.de>
2021-06-14 23:29:52 +0200

    init tauri react client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {World} from './components/game/World'
+
+function App() {
+  return (
+    <div className="App">
+      <p>Test</p>
+      <World></World>
+    </div>
+  );
+}
+
+export default App;
